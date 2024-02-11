415025a7eedbaa8747617667778dd145eb5c06f9
Nilachala Sahoo <113751624+nilachalasahoo@users.noreply.github.com>
2024-02-12 03:22:37 +0530

    submit form

--- js/script.js
+++ js/script.js
@@ -75,5 +75,20 @@ const nav=document.querySelector(".nav"),
         }
 
 // Submit form
-
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyLfHG7GMoogkvSMC808VunFPCqAt3Seni_vGwL272fJusLXonY0luX4J__1Xa_8iCF3g/exec'
+        const form = document.forms['submit-to-google-sheet']
+        const msg= document.getElementById("msg")
+      
+        form.addEventListener('submit', e => {
+          e.preventDefault()
+          fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+            .then(response => {
+                msg.innerHTML= "Message sent successfully"
+                setTimeout(function(){
+                    msg.innerHTML=""
+                },5000)
+                form.reset()
+            })
+            .catch(error => console.error('Error!', error.message))
+        })
 
